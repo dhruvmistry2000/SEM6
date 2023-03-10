b917285061675c2d36e60f95035acf1a08f86f99
Dhruv <dhruvmistry2000@gmail.com>
2023-03-10 10:38:08 +0530

    System Software

--- SS/prac1.py
+++ SS/prac1.py
@@ -1,17 +1,13 @@
-# create a file
 with open('example.txt', 'w') as f:
     f.write('Hello, world!\n')
 
-# read from the file
 with open('example.txt', 'r') as f:
     contents = f.read()
     print(contents)
 
-# write to the file
 with open('example.txt', 'a') as f:
     f.write('This is a new line.\n')
 
-# read from the file again
 with open('example.txt', 'r') as f:
     contents = f.read()
     print(contents)
